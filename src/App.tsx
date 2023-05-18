fdf7ae081ba6d6ba6d339924e3e489e198fb0dc4
Andrej Gajdos <agajdy@gmail.com>
2023-05-19 02:13:59 +0700

    fix: types

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import {
   ActionFunction,
 } from "react-router-dom";
 
-interface IRoute {
-  path: string;
-  Element: JSX.Element;
+interface RouteCommon {
   loader?: LoaderFunction;
   action?: ActionFunction;
-  ErrorBoundary?: JSX.Element;
+  ErrorBoundary?: React.ComponentType<any>;
 }
 
-const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+interface IRoute extends RouteCommon{
+  path: string;
+  Element: React.ComponentType<any>;
+}
+
+interface Pages {
+  [key: string]: {
+    default: React.ComponentType<any>;
+  } & RouteCommon
+}
+
+const pages: Pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
 
 const routes: IRoute[] = [];
 for (const path of Object.keys(pages)) {
@@ -28,23 +37,17 @@ for (const path of Object.keys(pages)) {
 
   routes.push({
     path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
-    // @ts-ignore
     Element: pages[path].default,
-    // @ts-ignore
-    loader: pages[path]?.loader as unknown as LoaderFunction | undefined,
-    // @ts-ignore
-    action: pages[path]?.action as unknown as ActionFunction | undefined,
-    // @ts-ignore
-    ErrorBoundary: pages[path]?.ErrorBoundary as unknown as JSX.Element,
+    loader: pages[path]?.loader as LoaderFunction | undefined,
+    action: pages[path]?.action as ActionFunction | undefined,
+    ErrorBoundary: pages[path]?.ErrorBoundary,
   });
 }
 
 const router = createBrowserRouter(
   routes.map(({ Element, ErrorBoundary, ...rest }) => ({
     ...rest,
-    // @ts-ignore
     element: <Element />,
-    // @ts-ignore
     ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
   }))
 );
